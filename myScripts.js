9325a235ab01b6c21b91d53c324f38ecf98df5cf
Chanisa Jayawardhana <sadin.chanisa8@gmail.com>
2023-05-18 19:00:58 +0530

    weather conditions
    Condition icons udpates

--- myScripts.js
+++ myScripts.js
@@ -1,5 +1,6 @@
 const apiKey="49a66178439b4560bd661234231605";
 const schtxt=document.querySelector("searchtxt");
+const weatherIcon=document.querySelector("weather-icon");
 
 async function weatherUpdate(city){
     const response=await fetch('http://api.weatherapi.com/v1/current.json?key='+ apiKey +'&q=' + city);
@@ -11,4 +12,14 @@ async function weatherUpdate(city){
     document.querySelector(".temp").innerHTML= Math.round(data.current.temp_c) +"°C";
     document.querySelector(".humidity").innerHTML= data.current.humidity + " %";
     document.querySelector(".wind").innerHTML= data.current.wind_kph + " km/h";
+    document.querySelector(".feels").innerHTML= data.current.feelslike_c +"°C";
+    document.querySelector(".condition").innerHTML= data.current.condition.text;
+
+   if(data.current.condition.text =="Patchy rain possible"){
+    weatherIcon.src = "//cdn.weatherapi.com/weather/64x64/night/176.png";
+
+   }else if(data.current.condition.text =="Partly cloudy"){
+    weatherIcon.src = "//cdn.weatherapi.com/weather/64x64/night/116.png";
+   }
+   
 }
