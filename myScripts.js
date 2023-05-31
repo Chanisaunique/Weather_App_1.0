f936b44a9517a1b62ef71b2277e66a6766a7f0e7
Chanisa Jayawardhana <sadin.chanisa8@gmail.com>
2023-05-31 21:42:38 +0530

    Home ,Without icons
    NO custom icons

--- myScripts.js
+++ myScripts.js
@@ -1,32 +1,35 @@
+const apiKey="49a66178439b4560bd661234231605";
+// const schtxt=document.querySelector("searchtext");
+// const weatherIcon=document.querySelector(".weather-icon");
 
-async function weatherUpdate(location){
+async function weatherUpdate(city){
     
-    const response=await fetch('http://api.weatherapi.com/v1/forecast.json?key='+ apiKey +'&q=' + location);
+    const response=await fetch('http://api.weatherapi.com/v1/forecast.json?key='+ apiKey +'&q=' + city);
     var data = await response.json();
 
     console.log(data);
 
     document.querySelector(".city").innerHTML= data.location.name;
     document.querySelector(".temp").innerHTML= Math.round(data.current.temp_c) +"°C";
-    document.querySelector(".humidity").innerHTML= data.current.humidity + " %";
-    document.querySelector(".wind").innerHTML= data.current.wind_kph + " km/h";
+    document.querySelector(".humidtytext").innerHTML= data.current.humidity + " %";
+    document.querySelector(".windtext").innerHTML= data.current.wind_kph + " km/h";
     document.querySelector(".feels").innerHTML= data.current.feelslike_c +"°C";
     document.querySelector(".condition").innerHTML= data.current.condition.text;
 
    if(data.current.condition.text =="Patchy rain possible"){
-    weatherIcon.src = "//cdn.weatherapi.com/weather/64x64/night/176.png";
+    weathericon.src = "//cdn.weatherapi.com/weather/64x64/night/176.png";
 
    }else if(data.current.condition.text =="Partly cloudy"){
-    weatherIcon.src = "//cdn.weatherapi.com/weather/64x64/night/116.png";
+    weathericon.src = "//cdn.weatherapi.com/weather/64x64/night/116.png";
 
    }else if(data.current.condition.text =="Sunny"){
-    weatherIcon.src = "//cdn.weatherapi.com/weather/64x64/day/113.png";
+    weathericon.src = "//cdn.weatherapi.com/weather/64x64/day/113.png";
    }
    else if(data.current.condition.text =="Clear"){
-    weatherIcon.src = "//cdn.weatherapi.com/weather/64x64/night/113.png";
+    weathericon.src = "//cdn.weatherapi.com/weather/64x64/night/113.png";
    }
    else if(data.current.condition.text =="Mist"){
-    weatherIcon.src = "//cdn.weatherapi.com/weather/64x64/night/143.png";
+    weathericon.src = "//cdn.weatherapi.com/weather/64x64/night/143.png";
    }
 
 }
